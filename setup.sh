47ed842a9947d06000051a9ad57dc0dcc91f7878
jaeho <ejaho0103@yonsei.ac.kr>
2024-04-05 00:52:02 -0400

    v0.1 commit

--- setup.sh
+++ setup.sh
@@ -1,29 +1,31 @@
-#!bin/zsh
+#!/bin/zsh
 
 # If zsh is not installed, notify and install it
-if ! [ -x "$(command -v zsh)" ]; then
+if ! [[ -x "$(command -v zsh)" ]]; then
   echo 'Error: zsh is not installed.' >&2
   echo ' Please run this script again after install zsh and oh-my-zsh.' >&2
   exit 1
 fi
 
 # if zsh is not the default shell notify and change it when user type yes
-if [ "$SHELL" != "/bin/zsh" ]; then
+if [[ "$SHELL" != "$(which zsh)" ]]; then
   echo "Your default shell is not zsh. Do you want to change it? (yes/no)"
-  read answer
-  if [ "$answer" == "yes" ]; then
-    chsh -s $(which zsh)
-  elif [ "$answer" == "no" ]; then
-    echo "Please run this script again after changing the default shell to zsh."
-    exit 1
-  else
-    echo "Please answer yes or no."
-    exit 1
-  fi
+  while true; do
+    read answer
+    if [[ "$answer" == "yes" ]]; then
+      chsh -s $(which zsh)
+      break
+    elif [[ "$answer" == "no" ]]; then
+      echo "Please run this script again after changing the default shell to zsh."
+      exit 1
+    else
+      echo "Please answer yes or no."
+    fi
+  done
 fi
 
 # Install oh-my-zsh if it is not installed
-if [ ! -d ~/.oh-my-zsh ]; then
+if [[ ! -d ~/.oh-my-zsh ]]; then
   echo "Installing oh-my-zsh..."
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
@@ -31,15 +33,15 @@ fi
 
 
 # It tmux is not installed, notify and install it
-if ! [ -x "$(command -v tmux)" ]; then
+if ! [[ -x "$(command -v tmux)" ]]; then
   echo "Tmux is not installed. Do you want to install it? (yes/no)"
   while true; do
     read answer
-    if [ "$answer" == "yes" ]; then
+    if [[ "$answer" == "yes" ]]; then
       echo "Installing tmux..."
       sudo apt-get install tmux
       break
-    elif [ "$answer" == "no" ]; then
+    elif [[ "$answer" == "no" ]]; then
       echo "Please run this script again after installing tmux."
       exit 1
     else
@@ -48,64 +50,89 @@ if ! [ -x "$(command -v tmux)" ]; then
   done
 fi
 
-# Install tmux tpm
-echo "Installing tmux tpm..."
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# Install tmux tpm if it is not installed
+if [[ ! -d ~/.tmux/plugins/tpm ]]; then
+  echo "Installing tmux tpm..."
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 
-echo "Creating symbolic links..."
-ln -s ~/.local/initial-settings/tmux/tmux.conf ~/.tmux.conf
+# if there is .tmux.conf file, ask to remove it and create symbolic link
+# and if the answer is neither yes nor no, ask again
+if [[ -f ~/.tmux.conf ]]; then
+  echo "There is a ~/.tmux.conf file. Do you want to remove it and apply my setting? (yes/no)"
+  while true; do
+    read answer
+    if [[ "$answer" == "yes" ]]; then
+      echo "Creating symbolic links..."
+      rm ~/.tmux.conf
+      /bin/ln -s ~/.local/initial-settings/tmux/tmux.conf ~/.tmux.conf
+      break
+    elif [[ "$answer" == "no" ]]; then
+      echo "Please remove the ~/.tmux.conf file and run the script again."
+      exit 1
+    else
+      echo "Please answer yes or no."
+    fi
+  done
+fi
 
 # Install neovim if it is not installed
-if ! [ -x "$(command -v nvim)" ]; then
+if ! [[ -x "$(command -v nvim)" ]]; then
   echo "Neovim is not installed. Do you want to install it? (yes/no)"
-    while true; do
-  read answer
-  if [ "$answer" == "yes" ]; then
-    echo "Installing neovim (v0.9.5)..."
-    cd neovim
-    wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
-    tar xzf nvim-linux64.tar.gz
-    echo "alias nvim='~/.local/initial-settings/neovim/nvim-linux64/bin/nvim'" >> ~/.zshrc
-    echo "export PATH=$PATH:~/.local/initial-settings/neovim/nvim-linux64/bin" >> ~/.zshrc
-    export PATH=$PATH:~/.local/initial-settings/neovim/nvim-linux64/bin
+  while true; do
+    read answer
+    if [[ "$answer" == "yes" ]]; then
+      echo "Installing neovim (v0.9.5)..."
+      cd neovim
+      wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
+      tar xzf nvim-linux64.tar.gz
+      echo "alias nvim='$HOME/.local/initial-settings/neovim/nvim-linux64/bin/nvim'" >> ~/.zshrc
+      echo "alias vim='nvim'" >> ~/.zshrc
+      export PATH=\$PATH:~/.local/initial-settings/neovim/nvim-linux64/bin
 
-    echo "Finished installing neovim"
-    echo "If you want to use neovim as default vim editor, run the following command:\necho "alias vim='nvim'" >> ~/.zshrc"
-  elif [ "$answer" == "no" ]; then
-    echo "Please run this script again after installing neovim."
-    exit 1
-  else 
-    echo "Please answer yes or no."
-    exit 1
-  fi
+      echo "Finished installing neovim"
+      break
+    elif [[ "$answer" == "no" ]]; then
+      echo "Please run this script again after installing neovim."
+      exit 1
+    else 
+      echo "Please answer yes or no."
+    fi
+  done
 fi
+
 
 # if there is .config/nvim directory, ask to remove it and create symbolic link
 # and if the answer is neither yes nor no, ask again
 # if the answer is yes, remove the directory and create symbolic link
 # if the answer is no, notify the user to remove the directory and run the script again
-if [ -d ~/.config/nvim ]; then
+if [[ -d ~/.config/nvim ]]; then
   echo "There is a ~/.config/nvim directory. Do you want to remove it and create a symbolic link? (yes/no)"
-  read answer
-  if [ "$answer" == "yes" ]; then
-    echo "Creating symbolic links for neovim..."
-    rm -rf ~/.config/nvim
-    ln -s ~/.local/initial-settings/neovim/nvim-settings ~/.config/nvim
-    echo "All done!"
-  elif [ "$answer" == "no" ]; then
-    echo "Please remove the ~/.config/nvim directory and run the script again."
-    exit 1
-  else
-    echo "Please answer yes or no."
-    exit 1
-  fi
+  while true; do
+    read answer
+    if [[ "$answer" == "yes" ]]; then
+      echo "Creating symbolic links for neovim..."
+      rm -rf ~/.config/nvim
+      /bin/ln -s ~/.local/initial-settings/neovim/nvim-settings ~/.config/nvim
+      echo "All done!"
+      break
+    elif [[ "$answer" == "no" ]]; then
+      echo "Please remove the ~/.config/nvim directory and run the script again."
+      exit 1
+    else
+      echo "Please answer yes or no."
+    fi
+  done
 else
-  ln -s ~/.local/initial-settings/nvim ~/.config/nvim
+  echo "Creating symbolic links for neovim..."
+  /bin/ln -s ~/.local/initial-settings/neovim/nvim-settings ~/.config/nvim
+  echo "All done!"
 fi
 
 echo "\n"
-echo "Please run the tmux and 'F2+Shift+I' to install the settings"
-echo "Please run the nvim and run :MasonIntallAll to install the rest of the plugins"
+echo "[IMPORTANT SETTING!!]"
+echo "Please run the tmux and press:\n\t'F2+Shift+I'\nto install the settings\n"
+echo "Please run the vim and run:\n\t:MasonIntallAll\nto install the rest of the plugins"
 echo "Read the https://github.com/NvChad/NvChad/tree/v2.5 to get more information about nvim settings"
 
 
