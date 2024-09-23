6404d543f0f17cc2ddc15fbda4fcd7863fa9918c
Jaeho Lee <ejaho0103@gmail.com>
2024-09-23 18:37:48 +0900

    Fix sudo and update nvim to v0.10.1

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #!/bin/zsh
 
+# Function to check if sudo is available
+use_sudo() {
+  if sudo -n true 2>/dev/null; then
+    echo "sudo"
+  else
+    echo ""
+  fi
+}
+
+SUDO=$(use_sudo)
+
 # If zsh is not installed, notify and install it
 if ! [[ -x "$(command -v zsh)" ]]; then
   echo 'Error: zsh is not installed.' >&2
@@ -7,7 +18,7 @@ if ! [[ -x "$(command -v zsh)" ]]; then
   exit 1
 fi
 
-# if zsh is not the default shell notify and change it when user type yes
+# If zsh is not the default shell, notify and change it when user types yes
 if [[ "$SHELL" != "$(which zsh)" ]]; then
   echo "Your default shell is not zsh. Do you want to change it? (yes/no)"
   while true; do
@@ -30,16 +41,14 @@ if [[ ! -d ~/.oh-my-zsh ]]; then
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-
-
-# It tmux is not installed, notify and install it
+# If tmux is not installed, notify and install it
 if ! [[ -x "$(command -v tmux)" ]]; then
   echo "Tmux is not installed. Do you want to install it? (yes/no)"
   while true; do
     read answer
     if [[ "$answer" == "yes" ]]; then
       echo "Installing tmux..."
-      sudo apt-get install tmux
+      $SUDO apt-get install tmux
       break
     elif [[ "$answer" == "no" ]]; then
       echo "Please run this script again after installing tmux."
@@ -56,8 +65,7 @@ if [[ ! -d ~/.tmux/plugins/tpm ]]; then
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
-# if there is .tmux.conf file, ask to remove it and create symbolic link
-# and if the answer is neither yes nor no, ask again
+# If there is a .tmux.conf file, ask to remove it and create symbolic link
 if [[ -f ~/.tmux.conf ]]; then
   echo "There is a ~/.tmux.conf file. Do you want to remove it and apply my setting? (yes/no)"
   while true; do
@@ -82,14 +90,13 @@ if ! [[ -x "$(command -v nvim)" ]]; then
   while true; do
     read answer
     if [[ "$answer" == "yes" ]]; then
-      echo "Installing neovim (v0.9.5)..."
+      echo "Installing neovim (v0.10.1)..."
       cd neovim
-      wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
+      wget https://github.com/neovim/neovim/releases/download/v0.10.1/nvim-linux64.tar.gz
       tar xzf nvim-linux64.tar.gz
       echo "alias nvim='$HOME/.local/initial-settings/neovim/nvim-linux64/bin/nvim'" >> ~/.zshrc
       echo "alias vim='nvim'" >> ~/.zshrc
       export PATH=\$PATH:~/.local/initial-settings/neovim/nvim-linux64/bin
-
       echo "Finished installing neovim"
       break
     elif [[ "$answer" == "no" ]]; then
@@ -101,18 +108,14 @@ if ! [[ -x "$(command -v nvim)" ]]; then
   done
 fi
 
-
-# if there is .config/nvim directory, ask to remove it and create symbolic link
-# and if the answer is neither yes nor no, ask again
-# if the answer is yes, remove the directory and create symbolic link
-# if the answer is no, notify the user to remove the directory and run the script again
+# If there is a ~/.config/nvim directory, ask to remove it and create symbolic link
 if [[ -d ~/.config/nvim ]]; then
   echo "There is a ~/.config/nvim directory. Do you want to remove it and create a symbolic link? (yes/no)"
   while true; do
     read answer
     if [[ "$answer" == "yes" ]]; then
       echo "Installing nodejs and npm..."
-      sudo apt install -y nodejs npm
+      $SUDO apt install -y nodejs npm
       echo "Creating symbolic links for neovim..."
       /bin/rm -rf ~/.config/nvim
       /bin/ln -s ~/.local/initial-settings/neovim/nvim-settings ~/.config/nvim
@@ -133,8 +136,6 @@ fi
 
 echo "\n"
 echo "[IMPORTANT SETTING!!]"
-echo "Please run the tmux and press:\n\t'F2+Shift+I'\nto install the settings\n"
-echo "Please run the vim and run:\n\t:MasonIntallAll\nto install the rest of the plugins"
+echo "Please run tmux and press:\n\t'F2+Shift+I'\nto install the settings\n"
+echo "Please run vim and run:\n\t:MasonInstallAll\nto install the rest of the plugins"
 echo "Read the https://github.com/NvChad/NvChad/tree/v2.5 to get more information about nvim settings"
-
-
