88d6053bda11647ae6ee0f5649df3d5d0f79bd5b
Jaeho Lee <jaeho@yonsei.ac.kr>
2025-08-28 16:02:14 +0900

    Revive ctrl+/ to toggle comments in normal and visual modes

--- neovim/nvim-settings/lua/mappings.lua
+++ neovim/nvim-settings/lua/mappings.lua
@@ -18,4 +18,6 @@ map("i", "<down>", "<nop>", { noremap = true })
 -- {{{ Define the kaymappings
 map("n", "<C-e>", "3<C-e>", { noremap = true, silent = true })
 map("n", "<C-y>", "3<C-y>", { noremap = true, silent = true })
+map("n", "<C-_>", "gcc", { remap = true, silent = true, desc = "Toggle comment line" })
+map("x", "<C-_>", "gc", { remap = true, silent = true, desc = "Toggle comment (visual)" })
 -- }}}
